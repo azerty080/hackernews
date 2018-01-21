756d0e0a6f9ebc28b2e943ea0e6d9dbb29cd4dc6
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-21 23:03:53 +0100

    added names to routes and added new routes for add, update and delete for articles

--- routes/web.php
+++ routes/web.php
@@ -11,33 +11,45 @@
 |
 */
 
-Route::get('/', 'PagesController@index');
+Route::get('/', 'PagesController@index')->name('index');
+Route::get('/home', 'PagesController@index')->name('home');
 
-
-Route::get('/home', 'PagesController@index');
-
-
-Route::get('/instructies', 'PagesController@instructies');
-
-
-Route::get('/login', 'PagesController@login');
-
-
-Route::get('/register', 'PagesController@register');
+Route::get('/instructies', 'PagesController@instructies')->name('instructies');
 
 
 
 
-Route::get('/article/add', 'ArticlesController@add');
+Route::get('/article/add', 'ArticlesController@add')->name('add_article');
+Route::post('/article/add', 'ArticlesController@store')->name('');
 
-Route::get('/article/edit/{id}', 'ArticlesController@edit');
+
+Route::get('/article/edit/{id}', 'ArticlesController@edit')->name('edit_article');
+Route::post('/article/edit/{id}', 'ArticlesController@update')->name('');
+
+
+Route::get('/article/delete/{id}', 'ArticlesController@delete')->name('delete_article');
+Route::post('/article/delete/{id}', 'ArticlesController@remove')->name('');
 
 
 
 
 Route::get('/comments/{id}', 'CommentsController@edit');
 
-Route::get('/comments/edit/{id}', 'CommentsController@edit');
+
+
+
+Route::get('/comments/add/{id}', 'CommentsController@add')->name('add_comment');
+Route::post('/comments/add/{id}', 'CommentsController@add')->name('');
+
+
+Route::get('/comments/edit/{id}', 'CommentsController@edit')->name('edit_comment');
+Route::post('/comments/edit/{id}', 'CommentsController@edit')->name('');
+
+
+Route::get('/comments/delete/{id}', 'CommentsController@delete')->name('delete_comment');
+Route::post('/comments/delete/{id}', 'CommentsController@delete')->name('');
+
+
 
 
 Auth::routes();
