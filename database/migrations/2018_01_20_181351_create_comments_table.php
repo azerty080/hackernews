1cb440dccfa58f5d54cdf829202b141eec236721
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-22 02:49:48 +0100

    changed name of value in create_comments_table

--- database/migrations/2018_01_20_181351_create_comments_table.php
+++ database/migrations/2018_01_20_181351_create_comments_table.php
@@ -17,7 +17,7 @@ class CreateCommentsTable extends Migration
             $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->integer('article_id')->unsigned()->index();
-            $table->text('text');
+            $table->text('body');
             $table->timestamps();
             $table->softDeletes();
 
