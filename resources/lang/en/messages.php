bf6d1d2d155b1799b9c7f1e26aa22d9c2b9bafdb
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 17:00:44 +0100

    added comment_success to messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -4,5 +4,6 @@ return [
     'article_success' => 'article ":title" :action succesfully',
     'denied' => 'you can\'t :action :item that is not yours',
     'confirm' => 'are you sure you cant to delete this :item?',
-    'voted' => 'you have :action ":title"'
+    'voted' => 'you have :action ":title"',
+    'comment_success' => 'comment :action succesfully'
 ];
