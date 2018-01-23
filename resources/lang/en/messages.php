71dcd63afdfad85b3e21a65355f21731a28086d2
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 16:12:31 +0100

    added confirm to messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,5 +2,6 @@
 
 return [
     'article_success' => 'article ":title" :action succesfully',
-    'denied' => 'you can\'t :action :item that is not yours'
+    'denied' => 'you can\'t :action :item that is not yours',
+    'confirm' => 'are you sure you cant to delete this :item?'
 ];
