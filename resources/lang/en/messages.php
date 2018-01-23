a88dcad2f2e3aa4cf76df78338ebc90607f20f30
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 16:14:49 +0100

    added voted to messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,5 +3,6 @@
 return [
     'article_success' => 'article ":title" :action succesfully',
     'denied' => 'you can\'t :action :item that is not yours',
-    'confirm' => 'are you sure you cant to delete this :item?'
+    'confirm' => 'are you sure you cant to delete this :item?',
+    'voted' => 'you have :action ":title"'
 ];
