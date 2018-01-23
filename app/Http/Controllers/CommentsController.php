347f06a48f2f5fadb2cf1c599aa741395b9e2b56
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 16:19:50 +0100

    added use paths

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Article;
+use App\Comment;
+use App\Http\Requests\CommentRequest;
 use Illuminate\Http\Request;
 
 class CommentsController extends Controller
