7d550b059fcb49ac6f4750f2c845fd7f6f7c48a8
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 17:58:22 +0100

    added error messaging

--- resources/views/Articles/add.blade.php
+++ resources/views/Articles/add.blade.php
@@ -4,62 +4,48 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Add article</div>
+            <div class="col-md-10 col-md-offset-1">
 
-                <div class="panel-body">
+                <div class="breadcrumb">
+                    <a href="{{ route('index') }}">← back to overview</a>
+                </div>
 
 
-                    <!--
+                @include('errors.errors')
 
-                    ALS ER EEN ERROR IS
 
-                    <div class="alert alert-danger">
-                        <strong>Whoops! Something went wrong!</strong>
 
-                        <br><br>
+                <div class="panel panel-default">
+                    <div class="panel-heading">Add article</div>
+                    <div class="panel-content">
+                        <form class="form-horizontal" method="POST" action="{{ route('store_article') }}">
+                            {{ csrf_field() }}
 
-                        <ul>
-                            <li>The title field is required.</li>
-                            <li>The url field is required.</li>
-                        </ul>
+                            <div class="form-group">
+                                <label for="title" class="col-sm-3 control-label">Title (max. 255 characters)</label>
+
+                                <div class="col-sm-6">
+                                    <input class="form-control" id="title" name="title" type="text" maxlength="255" value="{{ old('title') }}">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="url" class="col-sm-3 control-label">URL</label>
+
+                                <div class="col-sm-6">
+                                    <input id="url" type="text" class="form-control" name="url">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-sm-6">
+                                    <button type="submit" class="btn btn-primary">
+                                        Add Article
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-
-                    <div class="breadcrumb">
-                        <a href="/">← back to overview</a>
-                    </div>
-                    -->
-
-
-
-
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group">
-                            <label for="title" class="col-sm-3 control-label">Title (max. 255 characters)</label>
-
-                            <div class="col-sm-6">
-                                <input id="title" type="text" class="form-control" name="title">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="url" class="col-sm-3 control-label">URL</label>
-
-                            <div class="col-sm-6">
-                                <input id="url" type="text" class="form-control" name="url">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-primary">
-                                    Add Article
-                                </button>
-                            </div>
-                        </div>
-
                 </div>
             </div>
         </div>
