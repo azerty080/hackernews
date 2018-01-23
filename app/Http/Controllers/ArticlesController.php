9ecee5e4751278eee8ff542ac1fca53191cda9cb
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 16:16:00 +0100

    added votes and session flashes to ArticlesController

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
+use App\Article;
+use App\Vote;
 use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
@@ -9,14 +12,26 @@ class ArticlesController extends Controller
 {
 	public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth', ['except' => ['index', 'show']]);
     }
+
+
 
 
     public function index()
     {
-    	return view('home');
+    	$articles = Article::all()-sortByDesc('points');
+
+    	return view('home', compact('articles'));
     }
+
+
+    public function show(Article $article)
+    {
+    	return view('articles.show', compact('article'));
+    }
+
+
 
 
     public function add()
@@ -32,14 +47,114 @@ class ArticlesController extends Controller
     	$article->points = 0;
     	$article->save();
 
+        session()->flash('success', trans('messages.article_success', ['action' => 'created', 'title' => $article->title]));
 
-
-
+    	return redirect()->route('index');
     }
 
 
-    public function edit()
+
+
+    public function edit(Article $article)
     {
-        return view('articles.edit');
+    	if ($article->user_id == Auth::id())
+    	{
+    		return view('articles.edit', compact('article'));
+    	}
+    	else
+    	{
+            session()->flash('danger', trans('messages.denied', ['action' => 'edit', 'item' => 'an article']));
+
+    		return redirect()->route('index');
+    	}
+    }
+
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+    	if ($article->user_id == Auth::id())
+    	{
+    		$article->update($request->all());
+    	}
+    	else
+    	{
+            session()->flash('danger', trans('messages.denied', ['action' => 'edit', 'item' => 'an article']));
+    	}
+
+    	return redirect()->route('index');
+    }
+
+
+
+
+    public function delete(Article $article)
+    {
+        if ($article->user_id == Auth::id())
+        {
+            session()->flash('delete_article_confirm', trans('messages.confirm', ['item' => 'article']));
+
+            return back();
+        }
+    	else
+        {
+            session()->flash('danger', trans('messages.denied', ['action' => 'delete', 'item' => 'an article']));
+
+            return redirect()->route('index');
+        }
+    }
+
+
+    public function destroy(Article $article)
+    {
+        if ($article->user_id == Auth::id())
+        {
+            $article->delete();
+            $article->comments()->delete();
+
+            session()->flash('success', trans('messages.article_success', ['action' => 'deleted', 'title' => $article->title]));
+        }
+        else
+        {
+            session()->flash('danger', trans('messages.denied', ['action' => 'delete', 'item' => 'an article']));
+        }
+
+        return redirect()->route('index');
+    }
+
+
+    public function cancel_delete()
+    {
+        return back();
+    }
+
+
+
+
+    public function upvote(Article $article)
+    {
+    	$article->increment('points');
+        $vote = new Vote;
+        $vote->user_id = Auth::id();
+        $vote->article_id = $article->id;
+        $vote->type = "up";
+        $vote->save();
+        
+        session()->flash('success', trans('messages.voted', ['action' => 'upvoted', 'title' => $article->title]));
+
+        return back();
+    }
+
+    public function downvote(Article $article)
+    {
+        $article->decrement('points');
+        $vote = new Vote;
+        $vote->user_id = Auth::id();
+        $vote->article_id = $article->id;
+        $vote->type = "down";
+        $vote->save();
+
+        session()->flash('success', trans('messages.voted', ['action' => 'upvoted', 'title' => $article->title]));
+
+        return back();
     }
 }
