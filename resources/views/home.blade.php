beca1e9ff15ecc00d7221bb9d1ffd33c186347c3
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-01-23 13:57:58 +0100

    added functionality to show articles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,52 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Article Overview</div>
 
-                <div class="panel-body">
-                    een artikel
+                <div class="panel-content">
+                    <ul class="article-overview">
+                        @foreach ($articles as $article)
+                            <li>
+                                <div class="vote">
+                                    @if (Auth::guest())
+                                        <div class="form-inline upvote">
+                                            <i class="fa fa-btn fa-caret-up disabled upvote" title="you need to be logged in to upvote"></i>
+                                        </div>
+                                        <div class="form-inline upvote">
+                                            <i class="fa fa-btn fa-caret-down disabled downvote" title="you need to be logged in to downvote"></i>
+                                        </div>
+                                    @else                                     
+                                        <form action="{{ route('upvote_article', ['article' => $article->id]) }}" method="POST" class="form-inline upvote">
+                                            {{ csrf_field() }}
+                                            {{ method_field('PATCH') }}
+
+                                            <button>
+                                                <i class="fa fa-btn fa-caret-up" title="upvote"></i>
+                                            </button>
+                                        </form>
+
+                                        <form action="{{ route('upvote_article', ['article' => $article->id]) }}" method="POST" class="form-inline downvote">
+                                            {{ csrf_field() }}
+                                            {{ method_field('PATCH') }}
+
+                                            <button>
+                                                <i class="fa fa-btn fa-caret-down" title="downvote"></i>
+                                            </button>
+                                        </form>
+                                    @endif
+                                </div>
+
+                                <div class="url">
+                                    <a href="{{ url($article->url) }}" class="urlTitle">{{ $article->title }}</a>
+                                         @if ($article->user_id == Auth::id())
+                                            <a href="{{ route('edit_article', ['article' => $article->id]) }}" class="btn btn-primary btn-xs edit-btn">edit</a>
+                                        @endif
+                                </div> 
+                                
+                                <div class="info">
+                                    {{ $article->points }} points | posted by {{ $article->user->name }} | <a href="{{ route('show_article', ['article' => $article->id]) }}">{{ $article->comments->count() }} comments</a>
+                                </div>
+                            </li>
+                        @endforeach
+                    </ul>
                 </div>
             </div>
         </div>
